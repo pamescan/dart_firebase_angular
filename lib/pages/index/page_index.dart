4d7a8b9ec25099f376e1ce0e36cf847729b42fab
pamescan <pamescan@gmail.com>
2017-03-11 11:47:28 +0100

    loading animation

--- lib/pages/index/page_index.dart
+++ lib/pages/index/page_index.dart
@@ -2,12 +2,13 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 
 @Component(
   selector: 'page-index',
   styleUrls: const ['page_index.css'],
   templateUrl: 'page_index.html',
-  directives: const [],
+  directives: const [ ROUTER_DIRECTIVES],
   providers: const [],
 )
 class PageIndex {}
